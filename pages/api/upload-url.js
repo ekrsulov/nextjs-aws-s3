9bfc63ffeec79cea1cb5674dc8bfddcb1020b86e
Ernesto Krsulovic <ernesto.krsulovic@gmail.com>
2021-03-15 16:18:24 -0300

    Update upload-url.js

--- pages/api/upload-url.js
+++ pages/api/upload-url.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
     region: process.env.REGION,
-    endpoint: "https://yenda.sfo3.digitaloceanspaces.com",
+    endpoint: "https://" + process.env.BUCKET_NAME + "." + process.env.REGION + ".digitaloceanspaces.com",
     signatureVersion: 'v4',
   });
 
